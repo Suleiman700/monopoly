348b9e7098ebe1e1961f55dc9071fca841e6b68e
Suleiman700 <soleman630@gmail.com>
2021-01-19 04:47:51 +0100

    Delete inc/translate directory

--- inc/translate/setLang_ar.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["lang_ar"]);
-$_SESSION["lang_ar"] = true;
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-?>
--- inc/translate/setLang_en.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["lang_ar"]);
-//$_SESSION["lang_ar"] = false;
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-?>
